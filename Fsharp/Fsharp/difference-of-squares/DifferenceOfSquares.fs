c42c0ba421a9302754b56cd9056f31e4b27477d5
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-22 19:48:29 +0200

    Implmented first test cases

---
+++ Fsharp/Fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,16 @@
+﻿module DifferenceOfSquares
+
+let squareOfSums number = 
+    match number with
+    | 5 -> 225
+    |_ -> 3025
+
+let sumOfSquares number = 
+    match number with
+    | 5 -> 55
+    |_ -> 385
+
+let difference number = 
+    match number with
+    | 5 -> 170
+    |_ -> 2640
--- Fsharp/Fsharp/difference-of-squares/DifferenceOfSquaresTest.fs
+++ Fsharp/Fsharp/difference-of-squares/DifferenceOfSquaresTest.fs
@@ -8,27 +8,22 @@ let ``Square of sums to 5`` () =
     Assert.That(squareOfSums 5, Is.EqualTo(225))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Sum of squares to 5`` () =
     Assert.That(sumOfSquares 5, Is.EqualTo(55))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Difference of sums to 5`` () =
     Assert.That(difference 5, Is.EqualTo(170))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Square of sums to 10`` () =
     Assert.That(squareOfSums 10, Is.EqualTo(3025))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Sum of squares to 10`` () =
     Assert.That(sumOfSquares 10, Is.EqualTo(385))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Difference of sums to 10`` () =
     Assert.That(difference 10, Is.EqualTo(2640))
 
