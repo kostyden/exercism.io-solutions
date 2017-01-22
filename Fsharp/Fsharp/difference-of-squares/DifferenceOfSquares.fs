d3f6ab469e902f6e6ce8e678937dc2a7b4ff9617
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-22 20:26:50 +0200

    Refactored - power two exctracted to the dedicated function

--- Fsharp/Fsharp/difference-of-squares/DifferenceOfSquares.fs
+++ Fsharp/Fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -3,12 +3,14 @@
 let private generateSequenceTo number =
     {1..number}
 
+let private powerTwo number =
+    number * number
+
 let squareOfSums number = 
-    let sum = generateSequenceTo number |> Seq.sum
-    sum * sum
+    generateSequenceTo number |> Seq.sum |> powerTwo
 
 let sumOfSquares number = 
-    generateSequenceTo number |> Seq.sumBy(fun value -> value * value)
+    generateSequenceTo number |> Seq.sumBy(fun value -> powerTwo value)
 
 let difference number = 
     squareOfSums number - sumOfSquares number
