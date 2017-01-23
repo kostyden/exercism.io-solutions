be366f0761a0cb5abb91c4512be2a586a3ec04fa
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-23 22:48:53 +0200

    Refactored to use String.map method

--- Fsharp/Fsharp/rna-transcription/RNATranscription.fs
+++ Fsharp/Fsharp/rna-transcription/RNATranscription.fs
@@ -1,10 +1,28 @@
 ﻿module RNATranscription
 
-let toRna dna =
-    match dna with
-    | "C" -> "G"
-    | "T" -> "A"
-    | "A" -> "U"
-    | "G" -> "C"
-    |_ -> "UGCACCAGAAUU"
+[<Literal>]
+let ADENINE = 'A'
+
+[<Literal>]
+let CYTOSINE = 'C'
+
+[<Literal>]
+let GUANINE = 'G'
+
+[<Literal>]
+let THYMINE = 'T'
+
+[<Literal>]
+let URACIL = 'U'
+
+let private toRNAComplement nucleotide = 
+    match nucleotide with
+    | CYTOSINE -> GUANINE
+    | THYMINE -> ADENINE
+    | ADENINE -> URACIL
+    | GUANINE -> CYTOSINE
+    |_ -> failwith "Not recognised nucleotide"    
+
+let toRna dnaSequence =
+    dnaSequence |> String.map toRNAComplement
 
