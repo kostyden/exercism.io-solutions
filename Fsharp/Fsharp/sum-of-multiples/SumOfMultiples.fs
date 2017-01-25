58515c2b2c34b57597578e59d09613506e3d9fda
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-25 06:53:46 +0200

    Temporary rafactoring

--- Fsharp/Fsharp/sum-of-multiples/SumOfMultiples.fs
+++ Fsharp/Fsharp/sum-of-multiples/SumOfMultiples.fs
@@ -1,4 +1,15 @@
 ﻿module SumOfMultiples
 
-let sumOfMultiples numbers number = 
-    failwith "Not implemented yet"
+let haveMultipleFor possibleMultiple number  = 
+    possibleMultiple % number = 0
+
+let isMultipleForNumbers numbers possibleMultiple  = 
+    numbers |> Seq.exists(haveMultipleFor possibleMultiple)
+
+let possibleMultiples maxMultiple =
+    {1 .. maxMultiple - 1}
+
+let sumOfMultiples numbers maxMultiple =    
+    possibleMultiples maxMultiple 
+        |> Seq.where(isMultipleForNumbers numbers) 
+        |> Seq.sum
