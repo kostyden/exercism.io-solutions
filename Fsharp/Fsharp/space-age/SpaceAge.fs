3108105aaa747c3cfc82a98dbcd321e6667d2d3d
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-27 21:56:43 +0200

    AddedSpaceAge files

--- Fsharp/Fsharp/space-age/SpaceAge.fs
+++ Fsharp/Fsharp/space-age/SpaceAge.fs
@@ -1,6 +1,6 @@
 ﻿module SpaceAge
 
-type Planet = Mercury | Venus | Earth| Mars | Jupiter | Saturn | Neptune | Uranus
+type Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Neptune | Uranus
 
 let spaceAge planet seconds = 
     failwith "Not implemented yet"
