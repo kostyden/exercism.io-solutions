e528bc32f11f795c2e19ecbf40ded5e6d284f779
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-27 14:30:33 +0200

    Added NUnit package and LeapYear solutions

---
+++ Fsharp/Fsharp/leap/LeapYear.fs
@@ -0,0 +1,14 @@
+﻿module LeapYear
+
+let isFourthCentury year = year % 400 = 0
+let isCentury year = year % 100 = 0
+let isFourthYear year = year % 4 = 0
+
+let isLeapYear year = 
+    match year with
+    | year when isFourthCentury year -> true
+    | year when isCentury year -> false
+    | year when isFourthYear year -> true
+    | _ -> false
+
+    
