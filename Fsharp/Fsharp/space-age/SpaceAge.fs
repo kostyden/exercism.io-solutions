24149ca5939b109875bf2e6cdc0ffe7ad473d80c
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-29 19:57:50 +0200

    SpaceAge refactored

--- Fsharp/Fsharp/space-age/SpaceAge.fs
+++ Fsharp/Fsharp/space-age/SpaceAge.fs
@@ -2,29 +2,34 @@
 
 open System
 
-let EARTH_YEAR_IN_SECONDS = 31557600m
-let MERCURY_YEAR_COEFFICENT = 0.2408467m
-let VENUS_YEAR_COEFFICENT = 0.61519726m
-let EARTH_YEAR_COEFFICENT = 1.0m
-let MARS_YEAR_COEFFICENT = 1.8808158m
-let JUPITER_YEAR_COEFFICENT = 11.862615m
-let SATURN_YEAR_COEFFICENT = 29.447498m
-let URANUS_YEAR_COEFFICENT = 84.016846m
-let NEPTUNE_YEAR_COEFFICENT = 164.79132m
-
 type Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune
 
-let spaceAge planet seconds = 
-    let solarYear = match planet with
-                    | Planet.Mercury -> MERCURY_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Venus -> VENUS_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Earth -> EARTH_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Mars -> MARS_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Jupiter -> JUPITER_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Saturn -> SATURN_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Uranus -> URANUS_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
-                    | Planet.Neptune -> NEPTUNE_YEAR_COEFFICENT * EARTH_YEAR_IN_SECONDS
+let private EARTH_SECONDS_PER_YEAR = 31557600m
 
-    Math.Round(seconds / solarYear, 2)
+let private orbitalPeriodCoefficentOf = 
+    function
+    | Planet.Mercury -> 0.2408467m
+    | Planet.Venus -> 0.61519726m
+    | Planet.Earth -> 1.0m
+    | Planet.Mars -> 1.8808158m
+    | Planet.Jupiter -> 11.862615m
+    | Planet.Saturn -> 29.447498m
+    | Planet.Uranus -> 84.016846m
+    | Planet.Neptune -> 164.79132m
+
+let private secondsPerYear orbitalPeriodCoefficent = 
+    orbitalPeriodCoefficent * EARTH_SECONDS_PER_YEAR
+
+let private convertSecondsToYears seconds secondsPerYear = 
+    seconds / secondsPerYear
+
+let private round (years : decimal) = Math.Round(years, 2)
+
+let spaceAge planet seconds = 
+    let convertAgeToSolarYears = convertSecondsToYears seconds
+    orbitalPeriodCoefficentOf planet 
+        |> secondsPerYear 
+        |> convertAgeToSolarYears 
+        |> round
 
 
