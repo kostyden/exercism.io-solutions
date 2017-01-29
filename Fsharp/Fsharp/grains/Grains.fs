acce6e5812cef41b50b2319f4fd5449cfcb7e870
kostyden <denys.kostynchuk@pp.inet.fi>
2017-01-29 21:38:10 +0200

    Grains - refactored

--- Fsharp/Fsharp/grains/Grains.fs
+++ Fsharp/Fsharp/grains/Grains.fs
@@ -2,11 +2,19 @@
 
 open System.Numerics
 
-let POWER = 2I
+let private BASE = 2I
 
-let square (number : int) = 
-    BigInteger.Pow(POWER, number - 1)
+let private exponentOf squareNumber = 
+    squareNumber - 1
+
+let private powWith exponent = 
+    BigInteger.Pow(BASE, exponent)
+
+let private chessBoard = {1 .. 64}
+
+let square number = 
+    powWith <| exponentOf number
 
 let total = 
-    {1 .. 64 } |> Seq.sumBy square
+    chessBoard |> Seq.sumBy square
 
