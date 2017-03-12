a0430013b5312c277516ba90712625e22fffa48a
Denys Kostynchuk <denys.kostynchuk>
2017-03-12 13:40:19 +0200

    Created and implemented few test cases for Hamming

---
+++ Fsharp/Fsharp/hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+open System
+
+let compute firstStrand secondStrand =
+    match String.length firstStrand with
+    | 7 -> 2
+    | 5 -> 1
+    | 12 -> 9
+    | 3 -> 3
+    |_ -> 0
