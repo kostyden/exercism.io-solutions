2149342df63ceed884f4bafd73fda1d961e995b0
Denys Kostynchuk <denys.kostynchuk>
2017-03-18 17:31:23 +0200

    Implementation with pattern matching

--- Fsharp/Fsharp/raindrops/Raindrops.fs
+++ Fsharp/Fsharp/raindrops/Raindrops.fs
@@ -1,5 +1,13 @@
 ﻿module Raindrops
 
 let convert number = 
-    string number
+    match number % 3, number % 5, number % 7 with
+    | 0,0,0 -> "PlingPlangPlong"
+    | 0,0,_ -> "PlingPlang"
+    | _,0,0 -> "PlangPlong"
+    | 0,_,0 -> "PlingPlong"
+    | 0,_,_ -> "Pling"
+    | _,0,_ -> "Plang"
+    | _,_,0 -> "Plong"
+    | _,_,_ -> string number
 
