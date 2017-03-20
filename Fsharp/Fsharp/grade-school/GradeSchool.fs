e2909c3f4519a90b3474ce03ad5619c15a94904e
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-20 18:05:39 +0200

    Added grade-school task files without ReadMe

--- Fsharp/Fsharp/grade-school/GradeSchool.fs
+++ Fsharp/Fsharp/grade-school/GradeSchool.fs
@@ -1,2 +1,14 @@
 ﻿module GradeSchool
 
+let empty () = 
+    failwith "Not implemented"
+
+let add name number school = 
+    failwith "Not implemented"
+
+let grade number school = 
+    failwith "Not implemented"
+
+let roster school = 
+    failwith "Not implemented"
+
