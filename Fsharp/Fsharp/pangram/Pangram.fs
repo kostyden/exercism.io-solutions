2c096ed18449e91dd55895ea23227a5b2d37d406
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-21 18:01:33 +0200

    pangram: implemented with Set.isSubset

--- Fsharp/Fsharp/pangram/Pangram.fs
+++ Fsharp/Fsharp/pangram/Pangram.fs
@@ -2,26 +2,11 @@
 
 open System
 
-let private alphabet = "abcdefghijklmnopqrstuvwxyz" |> Set.ofSeq
-
-let private isInAlphabet character = alphabet |> Set.contains character
-
-let private toAlphabetOption lowerCaseCharacter = 
-    match lowerCaseCharacter |> isInAlphabet with
-    | true -> Some lowerCaseCharacter
-    | false -> None
-
-let private alphabetCharacter character = character |> Char.ToLower |> toAlphabetOption
-
-let private isSameAsAlphabet characters = Seq.length characters = Seq.length alphabet
-
-let private toSetOfAlphabetCharacters input = 
-    input 
-    |> Seq.choose(alphabetCharacter)
-    |> Set.ofSeq
+let private alphabet = { 'a'..'z' } |> Set.ofSeq
     
 let isPangram input = 
     input
-    |> toSetOfAlphabetCharacters
-    |> isSameAsAlphabet
+    |> Seq.map Char.ToLower
+    |> Set.ofSeq
+    |> Set.isSubset alphabet
 
