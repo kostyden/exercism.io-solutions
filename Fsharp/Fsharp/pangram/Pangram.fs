8310ed61053165543118d7ff1a01fc5a20406dbc
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-21 17:52:02 +0200

    pangram: implemented all test cases

--- Fsharp/Fsharp/pangram/Pangram.fs
+++ Fsharp/Fsharp/pangram/Pangram.fs
@@ -1,4 +1,28 @@
 ﻿module Pangram
 
-let isPangram input = false
+open System
+
+let private alphabet = "abcdefghijklmnopqrstuvwxyz" |> Set.ofSeq
+
+let private isInAlphabet character = alphabet |> Set.contains character
+
+let private toAlphabetOption lowerCaseCharacter = 
+    match lowerCaseCharacter |> isInAlphabet with
+    | true -> Some lowerCaseCharacter
+    | false -> None
+
+let private alphabetCharacter character = character |> Char.ToLower |> toAlphabetOption
+
+let private isSameAsAlphabet characters = Seq.length characters = Seq.length alphabet
+
+let private toSetOfAlphabetCharacters input = 
+    input 
+    |> Seq.choose(alphabetCharacter)
+    |> Set.ofSeq
+    
+
+let isPangram input = 
+    input
+    |> toSetOfAlphabetCharacters
+    |> isSameAsAlphabet
 
