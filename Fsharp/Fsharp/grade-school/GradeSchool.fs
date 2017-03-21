59d2c1668d0832ed279d02f8190c75f4e1853946
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-21 16:22:39 +0200

    grade-school: refactored by extracting fst and snd to dedicated methods

--- Fsharp/Fsharp/grade-school/GradeSchool.fs
+++ Fsharp/Fsharp/grade-school/GradeSchool.fs
@@ -1,18 +1,25 @@
 ﻿module GradeSchool
 
+let private name student = student |> fst
+
+let private gradeNumber student = student |> snd
+
+let private isGradeSameAs givenGrade student = student |> gradeNumber = givenGrade    
+
 let empty = List.empty
 
 let add name gradeNumber school = 
     (name, gradeNumber) :: school
 
 let grade gradeNumber school = 
+    let isGivenGrade = isGradeSameAs gradeNumber
     school 
-    |> List.filter (fun (name, number) -> number = gradeNumber) 
-    |> List.map fst
+    |> List.filter isGivenGrade
+    |> List.map name
     |> List.sort
 
 let roster school = 
     school
-    |> List.groupBy snd
-    |> List.map (fun (grade, students) -> (grade, students |> List.map fst))
+    |> List.groupBy gradeNumber
+    |> List.map (fun (grade, students) -> (grade, students |> List.map name))
 
