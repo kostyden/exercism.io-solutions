5861b216195555c405b37b7f9734ca4f18ad73c9
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-21 16:43:46 +0200

    grade-school: introduced names function

--- Fsharp/Fsharp/grade-school/GradeSchool.fs
+++ Fsharp/Fsharp/grade-school/GradeSchool.fs
@@ -6,6 +6,8 @@ let private gradeNumber student = student |> snd
 
 let private isGradeSameAs givenGrade student = student |> gradeNumber = givenGrade    
 
+let private names students = students |> List.map name
+
 let empty = List.empty
 
 let add name gradeNumber school = 
@@ -15,12 +17,12 @@ let grade gradeNumber school =
     let isGivenGrade = isGradeSameAs gradeNumber
     school 
     |> List.filter isGivenGrade
-    |> List.map name
+    |> names
     |> List.sort
 
 let roster school = 
     school
     |> List.groupBy gradeNumber
-    |> List.map (fun (grade, students) -> (grade, students |> List.map name))
+    |> List.map (fun (grade, students) -> (grade, students |> names))
     |> List.sortBy fst
 
