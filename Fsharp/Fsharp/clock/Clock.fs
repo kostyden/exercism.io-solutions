6167e4cda4cd612e21739a24cb4499a778ca30d0
kostyden <denys.kostynchuk@pp.linet.fi>
2017-03-23 14:07:48 +0200

    clock: Added task files

---
+++ Fsharp/Fsharp/clock/Clock.fs
@@ -0,0 +1,14 @@
+﻿module Clock
+
+let mkClock hour minute = 
+    failwith "Not implemented yet"
+
+let add amount clock = 
+    failwith "Not implemented yet"
+
+let subtract amount clock = 
+    failwith "Not implemented yet"
+
+let display clock = 
+    failwith "Not implemented yet"
+
