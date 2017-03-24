7b13cfad872bda52694a484fe00ea4259a9d84a4
kostyden <denys.kostynchuk@pp.inet.fi>
2017-03-24 18:10:24 +0200

    clock: not ready implementation

--- Fsharp/Fsharp/clock/Clock.fs
+++ Fsharp/Fsharp/clock/Clock.fs
@@ -1,14 +1,25 @@
 ﻿module Clock
 
+type Clock = { Hours: int; Minutes: int }
+
 let mkClock hour minute = 
-    failwith "Not implemented yet"
+    { Hours = (hour + minute / 60) % 24; Minutes = minute % 60 }
 
-let add amount clock = 
-    failwith "Not implemented yet"
+let add minutes clock = 
+    let hours = (clock.Hours + (minutes / 60)) % 24
+    let minutesToAdd = minutes % 60
+    { Hours = hours; Minutes = clock.Minutes + minutesToAdd }
 
-let subtract amount clock = 
-    failwith "Not implemented yet"
+let private subtractHours current amount = 
+    match amount > current with
+    | false -> current - amount
+    | true -> 24 - ((amount - current) % 24)
+
+let subtract minutes clock = 
+    match minutes > clock.Minutes with
+    | false -> { clock with Minutes = clock.Minutes - minutes }
+    | true -> { Hours = subtractHours clock.Hours (1 + (minutes - clock.Minutes) / 60); Minutes = 60 - ((minutes - clock.Minutes) % 60) }
 
 let display clock = 
-    failwith "Not implemented yet"
+    sprintf "%02i:%02i" clock.Hours clock.Minutes
 
--- Fsharp/Fsharp/clock/ClockTest.fs
+++ Fsharp/Fsharp/clock/ClockTest.fs
@@ -10,87 +10,74 @@ let ``Prints 8 o'clock`` () =
     Assert.That(display clock, Is.EqualTo("08:00"))
 
 [<Test>]
-[<Ignore("Remove to run test")>]     
 let ``Prints 9 o'clock`` () =
     let clock = mkClock 9 0
     Assert.That(display clock, Is.EqualTo("09:00"))
 
 [<Test>]
-[<Ignore("Remove to run test")>]     
 let ``Can print single-digit minutes`` () =
     let clock = mkClock 11 9
     Assert.That(display clock, Is.EqualTo("11:09"))
 
 [<Test>]
-[<Ignore("Remove to run test")>]
 let ``Can print double-digit minutes`` () =
     let clock = mkClock 11 19
     Assert.That(display clock, Is.EqualTo("11:19"))
 
 [<Test>] 
-[<Ignore("Remove to run test")>]     
 let ``Can add minutes`` () =
     let clock = mkClock 10 0
     let added = clock |> add 3
     Assert.That(display added, Is.EqualTo("10:03"))
 
 [<Test>] 
-[<Ignore("Remove to run test")>]     
 let ``Can add over an hour`` () =
     let clock = mkClock 10 0
     let added = clock |> add 63
     Assert.That(display added, Is.EqualTo("11:03"))
 
 [<Test>]   
-[<Ignore("Remove to run test")>]   
 let ``Can add over more than one day`` () =
     let clock = mkClock 10 0
     let added = clock |> add 7224
     Assert.That(display added, Is.EqualTo("10:24"))
 
 [<Test>]   
-[<Ignore("Remove to run test")>]   
 let ``Can subtract minutes`` () =
     let clock = mkClock 10 3
     let subtracted = clock |> subtract 3
     Assert.That(display subtracted, Is.EqualTo("10:00"))
 
 [<Test>]   
-[<Ignore("Remove to run test")>]   
 let ``Can subtract to previous hour`` () =
     let clock = mkClock 10 3
     let subtracted = clock |> subtract 30
     Assert.That(display subtracted, Is.EqualTo("09:33"))
 
 [<Test>]  
-[<Ignore("Remove to run test")>]    
 let ``Can subtract over an hour`` () =
     let clock = mkClock 10 3
     let subtracted = clock |> subtract 70
     Assert.That(display subtracted, Is.EqualTo("08:53"))
 
 [<Test>]    
-[<Ignore("Remove to run test")>]  
 let ``Wraps around midnight`` () =
     let clock = mkClock 23 59
     let added = clock |> add 2
     Assert.That(display added, Is.EqualTo("00:01"))
 
 [<Test>]  
-[<Ignore("Remove to run test")>]    
 let ``Wraps around midnight backwards`` () =
     let clock = mkClock 0 1
     let subtracted = clock |> subtract 2
     Assert.That(display subtracted, Is.EqualTo("23:59"))
 
 [<Test>]   
-[<Ignore("Remove to run test")>]   
 let ``Midnight is zero hundred hours`` () =
     let clock = mkClock 24 0
     Assert.That(display clock, Is.EqualTo("00:00"))
 
 [<Test>]    
-[<Ignore("Remove to run test")>]  
 let ``Sixty minutes is next hour`` () =
     let clock = mkClock 1 60
     Assert.That(display clock, Is.EqualTo("02:00"))
